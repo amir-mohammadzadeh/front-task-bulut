113d2b16b442b4e5e834e53334272d7b7b839e5e
amir <amir.code98@gmail.com>
2024-02-28 16:41:40 +0330

    Create Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react'
+import './Card.css'
+
+interface Prop{
+    title: string,
+    value: string,
+    children: ReactNode
+}
+
+const Card = ({title , value, children: icon}:Prop) => {
+  return (
+    <div className='card_container'>
+        <span className='card-icon'>
+            {icon}
+        </span>
+
+        <h1 className='card-heading'>
+           {value}
+        </h1>
+        <span className="card-title">
+            {title}
+        </span>
+    </div>
+  )
+}
+
+export default Card
